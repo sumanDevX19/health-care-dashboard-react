a17f7df19445c0409a32ca77e4fc4625d2d77d34
sumanDevX19 <contactmrsumansaha@gmail.com>
2025-05-25 23:37:49 +0530

    25.05.25 : Adding and Setup of DashboardOverview Component

---
+++ src/Components/DashboardOverview.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AnatomySection from './AnatomySection'
+import '../DashboardOverview.css'
+import HealthStatusCards from './HealthStatusCards'
+
+const DashboardOverview = () => {
+  return (
+        <>
+          <div className='dashboard-overview' >
+            <AnatomySection/>
+            <HealthStatusCards/>
+
+          </div>
+        </>
+  )
+}
+
+export default DashboardOverview
