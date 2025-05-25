38acb5dd0d33b42808891ee2aa6171745cd9db78
sumanDevX19 <contactmrsumansaha@gmail.com>
2025-05-25 23:38:34 +0530

    25.05.25 : Setup of DashboardMainComponent Component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 import TopNavbar from './Components/TopNavbar'
 import SideBar from './Components/SideBar'
+import DashboardMainComponent from './Components/DashboardMainComponent'
 
 const App  = () => {
   return (
     <>
     <TopNavbar/>
+    <div style={{display:'flex'}}>
     <SideBar/>
+    <DashboardMainComponent/>
+    </div>
     </>
   )
 }
