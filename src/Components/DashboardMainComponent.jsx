85c8f039bd639bf389d682f0f90911c474ae7feb
sumanDevX19 <contactmrsumansaha@gmail.com>
2025-05-25 23:37:12 +0530

    25.05.25 : Adding and Setup of DashboardMainComponent Component

---
+++ src/Components/DashboardMainComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AnatomySection from './AnatomySection'
+import DashboardOverview from './DashboardOverview'
+
+const DashboardMainComponent = () => {
+  return (
+    <>
+        <DashboardOverview/>      
+    </>
+  )
+}
+
+export default DashboardMainComponent
